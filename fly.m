a7fc137d2b4491f5fa577034ac25cce2e2aa24e5
Robert Huston <robert.huston@pinpointdynamics.com>
2022-01-21 17:58:46 -0500

    Added grids to range,angle plots. Added more ticks to parametric plot.

--- fly.m
+++ fly.m
@@ -53,12 +53,16 @@ function fly(scenario)
     figure("name", "Parametric");
     plot(ez, nz, ".", ef, nf, "*", e, n, "o");
     axis([-50, 50, -50, 50], "square");
+    set(gca, 'xtick', -50:10:50);
+    set(gca, 'ytick', -50:10:50);
     set(gca(), "xgrid", "on", "ygrid", "on");
 
     figure("name", "Range");
     plot(t, rz, ".", t, rf, "*", t, r, "o");
+    set(gca(), "xgrid", "on", "ygrid", "on");
 
     figure("name", "Angle");
     plot(t, thetaz, ".", t, thetaf, "*", t, theta, "o");
+    set(gca(), "xgrid", "on", "ygrid", "on");
 
 endfunction
