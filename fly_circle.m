a887dd3d1ab7edd9917794766fa027cf14089a8b
Robert Huston <robert.huston@pinpointdynamics.com>
2022-01-21 17:47:15 -0500

    Restructured into single fly script with scenario files.

---
+++ fly_circle.m
@@ -0,0 +1,16 @@
+function [t, n, e] = fly_circle
+    tmax = 100;
+
+    t = [0 : 1 : tmax];
+
+    phi0 = 0;
+    w = 2 * pi / tmax;
+
+    nC = 5;
+    eC = 5;
+
+    pC = 25;
+
+    n = pC * cos(phi0 + w * t) + nC;
+    e = pC * sin(phi0 + w * t) + eC;
+endfunction
---
+++ fly_line.m
@@ -0,0 +1,12 @@
+function [t, n, e] = fly_line
+    t = [0 : 1 : 100];
+
+    n0 = 40;
+    vn = -1;
+
+    e0 = 50;
+    ve = -1;
+
+    n = n0 + vn * t;
+    e = e0 + ve * t;
+endfunction
