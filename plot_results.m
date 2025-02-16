10f84d1fd04c522cd44f09a3ffcb867d860071c9
Robert Huston <robert.huston@pinpointdynamics.com>
2025-02-16 15:13:28 -0500

    Combined component plots into subplot.

--- plot_results.m
+++ plot_results.m
@@ -10,20 +10,26 @@ function plot_results(label, t, n, e, r, theta, nz, ez, rz, thetaz, nf, vnf, ef,
     set(gca, 'ytick', -50:10:50);
     set(gca(), "xgrid", "on", "ygrid", "on");
 
-    figure("name", ["vn" " - " label]);
+    figure("name", ["Components" " - " label]);
+    
+    subplot(2,2,1)
     plot(t, vnf, ".-");
     set(gca(), "xgrid", "on", "ygrid", "on");
+    title("vn");
 
-    figure("name", ["ve" " - " label]);
+    subplot(2,2,2)
     plot(t, vef, ".-");
     set(gca(), "xgrid", "on", "ygrid", "on");
+    title("ve");
 
-    figure("name", ["Range" " - " label]);
+    subplot(2,2,3)
     plot(t, rz, ".", t, rf, "*", t, r, "o");
     set(gca(), "xgrid", "on", "ygrid", "on");
+    title("Range");
 
-    figure("name", ["Angle" " - " label]);
+    subplot(2,2,4)
     plot(t, thetaz, ".", t, thetaf, "*", t, theta, "o");
     set(gca(), "xgrid", "on", "ygrid", "on");
+    title("Angle");
 
 endfunction
